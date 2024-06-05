8f2d2d5355fc36d92502a2806a6364caa9f5f812
Joelmk28 <joelmuhindok@gmail.com>
2024-06-05 15:03:58 +0100

    message d'erreur apres tentive d'envoie du email 2

--- mail/contact.js
+++ mail/contact.js
@@ -27,7 +27,7 @@ $(function () {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
                     $('#success > .alert-success')
-                            .append("<strong>Your message has been sent. </strong>");
+                            .append("<strong>Your message has been sent. Call me or leave me a WhatsApp message if you find my email reply lying around. </strong>");
                     $('#success > .alert-success')
                             .append('</div>');
                     $('#contactForm').trigger("reset");
